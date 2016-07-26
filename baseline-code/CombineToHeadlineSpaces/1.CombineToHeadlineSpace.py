736d6bfdbd08c32e7a590c5b34d16eadc486f276
Nguyen Duc-Duy <ducduy.nguyen@studenti.unitn.it>
2016-07-26 10:25:34 +0000

    Add files via upload

---
+++ baseline-code/CombineToHeadlineSpaces/1.CombineToHeadlineSpace.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  5 17:59:22 2016
+
+This program aims to COMBINE the following parts into a SEMANTIC SPACE
+- The co-occurence matrix
+- The column file (loaded from core semantic space building steps)
+- The rows: which is the headlines (tagged)
+
+@author: duy
+"""
+
